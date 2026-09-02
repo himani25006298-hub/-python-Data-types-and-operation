d38119f68773f861dc6f6cceb7a272c09eaf4d62
himani25006298-hub <himani.25006298@jainuniversity.ac.in>
2026-09-02 08:57:07 +0530

    Add files via upload

---
+++ 25BCAR0561exp3.py
@@ -0,0 +1,15 @@
+s = "Hello Python"
+print("Original string:",s)
+print("length:",len(s))
+print("uppercase:",s.upper())
+print("lowercase:",s.lower())
+print("character at index 6:",s[6])
+print("position of python:",s.find ("Python"))
+print("slice:",s[6:])
+print("Replace:",s.replace("python","world"))
+print(s)
+print("contains Python:","Python" in s)
+print("concatenation:",s+"programming")
+s2 = " Hello world "
+print("Trim:",s2.strip())
+    
