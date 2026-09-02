db7d8338d9d6a633553215fa9e4acdba4648534e
himani25006298-hub <himani.25006298@jainuniversity.ac.in>
2026-09-02 08:54:55 +0530

    Add files via upload

--- 25BCAR0561.py
+++ 25BCAR0561.py
@@ -1,15 +1,41 @@
-s = "Hello Python"
-print("Original string:",s)
-print("length:",len(s))
-print("uppercase:",s.upper())
-print("lowercase:",s.lower())
-print("character at index 6:",s[6])
-print("position of python:",s.find ("Python"))
-print("slice:",s[6:])
-print("Replace:",s.replace("python","world"))
-print(s)
-print("contains Python:","Python" in s)
-print("concatenation:",s+"programming")
-s2 = " Hello world "
-print("Trim:",s2.strip())
-    
+# Calculator using control flow and looping statements
+
+while True:
+    print("\n--- CALCULATOR ---")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Exit")
+
+    choice = int(input("Enter your choice: "))
+
+    if choice == 5:
+        print("Calculator closed.")
+        break
+
+    if choice < 1 or choice > 5:
+        print("Invalid choice!")
+        continue
+
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    if choice == 1:
+        result = num1 + num2
+        print("Result =", result)
+
+    elif choice == 2:
+        result = num1 - num2
+        print("Result =", result)
+
+    elif choice == 3:
+        result = num1 * num2
+        print("Result =", result)
+
+    elif choice == 4:
+        if num2 == 0:
+            print("Cannot divide by zero!")
+        else:
+            result = num1 / num2
+            print("Result =", result)
